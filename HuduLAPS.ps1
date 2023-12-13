bfc63515a19ae460f999ccb27eccff0aa0948e8e
ethan flowerday <ethan@flowerday.me>
2023-12-12 19:29:58 -0500

    Create HuduLAPS.ps1
    init comit

---
+++ HuduLAPS.ps1
@@ -0,0 +1,79 @@
+#####################################################################
+$HuduAPIKey = "@huduAPI@"
+# Set the base domain of your Hudu instance without a trailing /
+$HuduBaseDomain = "@huduURL@"
+#Company Name as it appears in Hudu
+$CompanyName = "%CLIENT_NAME%"
+$NewAdminUsername = "@newAdminUsername@"
+$ChangeAdminUsername = $false
+#####################################################################
+
+
+if (Get-Module -ListAvailable -Name HuduAPI) {
+    Import-Module HuduAPI 
+} else {
+    Install-Module HuduAPI -Force
+    Import-Module HuduAPI
+}
+  
+#Set Hudu logon information
+New-HuduAPIKey $HuduAPIKey
+New-HuduBaseUrl $HuduBaseDomain
+
+$Company = Get-HuduCompanies -name $CompanyName
+if ($company) {	
+    #This is the data we'll be sending to Hudu
+		
+    $ComputerName = $($Env:COMPUTERNAME)
+			
+    #Find the parent asset from serial
+    $ParentAsset = Get-HuduAssets -primary_serial (get-ciminstance win32_bios).serialnumber
+			
+    #Check if there is only 1 matching asset
+    if (($ParentAsset | measure-object).count -ne 1) {
+        $ParentAsset = Get-HuduAssets -companyid $company.id -name $ComputerName
+    }
+
+    if (($ParentAsset | measure-object).count -eq 1) {
+
+        add-type -AssemblyName System.Web
+        #This is the process we'll be perfoming to set the admin account.
+        $LocalAdminPassword = [System.Web.Security.Membership]::GeneratePassword(24, 5)
+        If ($ChangeAdminUsername -eq $false) {
+            Set-LocalUser -name "Support" -Password ($LocalAdminPassword | ConvertTo-SecureString -AsPlainText -Force) -PasswordNeverExpires:$true
+        } else {
+            $ExistingNewAdmin = get-localuser | Where-Object { $_.Name -eq $NewAdminUsername }
+            if (!$ExistingNewAdmin) {
+                write-host "Creating new user" -ForegroundColor Yellow
+                New-LocalUser -Name $NewAdminUsername -Password ($LocalAdminPassword | ConvertTo-SecureString -AsPlainText -Force) -PasswordNeverExpires:$true
+                Add-LocalGroupMember -Group Administrators -Member $NewAdminUsername
+                Disable-LocalUser -Name "Support"
+            } else {
+                write-host "Updating admin password" -ForegroundColor Yellow
+                set-localuser -name $NewAdminUsername -Password ($LocalAdminPassword | ConvertTo-SecureString -AsPlainText -Force)
+            }
+        }
+        if ($ChangeAdminUsername -eq $false ) { $username = "Support" } else { $Username = $NewAdminUsername }
+
+
+        $PasswordObjectName = "$($Env:COMPUTERNAME) - Local Support Account"
+        $notes = "Local Admin Password for $($Env:COMPUTERNAME)"
+        # See if a password already exists
+        $password = Get-HuduPasswords -name $PasswordObjectName -companyid $company.id 
+	
+        if ($password) {
+            Write-Host "Updated Password"
+            $password = set-hudupassword -id $password.id -company_id $company.id -passwordable_type "Asset" -passwordable_id $ParentAsset.id -in_portal $false -password $LocalAdminPassword -description $notes -name $PasswordObjectName -username $username
+        } else {
+            Write-Host "Created Password"
+            $password = new-hudupassword -company_id $company.id -passwordable_type "Asset" -passwordable_id $ParentAsset.id -in_portal $false -password $LocalAdminPassword -description $notes -name $PasswordObjectName -username $username
+        }
+
+
+    } else {
+        Write-Error "A Matching Asset Could not be found"
+    }
+
+} else {
+    Write-Error "$CompanyName was not found in Hudu"
+}
